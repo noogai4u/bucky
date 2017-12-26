e83c3279d3803527aae58fd3bda5b6219fb1713d
Arvind A <noogai4u@users.noreply.github.com>
2017-12-27 01:37:10 +0530

    Buckys c++ practice.

---
+++ c++/2-simple calculator/simple calculator/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+
+    int sum;
+
+    cout << "enter the first number \n";
+    cin >> a;
+
+    cout << "enter the second number \n";
+    cin >> b;
+
+    cout << "enter the third number \n";
+    cin >> c;
+
+    cout << "enter the fourth number \n";
+    cin >> d;
+
+    sum = a + b + c + d;
+
+    cout << "the sum is \n" << sum << endl;
+
+    return 0;
+
+
+}
